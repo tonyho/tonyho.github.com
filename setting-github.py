582b93c21136788ebf56c12940db9ad089ebd4be
tonyho <373466062@qq.com>
2013-04-16 16:30:00 +0800

    User the bootstrap2 theme

--- setting-github.py
+++ setting-github.py
@@ -37,6 +37,13 @@ SIDEBAR_CUSTOM = r"""
 <li class="nav-header"><h4><i class="icon-list-alt"></i>Weibo</h4></li>
 <iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1132375127&verifier=b1fc848b&dpc=1"></iframe>
 """
+
+SIDEBAR_CUSTOM = r"""
+<li class="nav-header"><h4><i class="icon-list-alt"></i>Weibo</h4></li>
+<iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1132375127&verifier=b1fc848b&dpc=1"></iframe>
+"""
+
+
 # TWITTER_USERNAME = 'TonyHo'
  #SIDEBAR_CUSTOM = r"""
 #这个是farseerfc同学自己加的, 可以显示他的新浪微博内容, 有微博的话可以把这些加上~
